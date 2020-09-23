1444ec2b92ed917186d7054a3a8f49573a5c48c4
Siddhi <siddhitaori42@gmail.com>
2020-09-23 14:17:49 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("mymodal");
+
+var btn = document.getElementById("prafull");
+
+console.log("Loaded")
+
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
